505d15979ece6741a3b2951b15f4d104f43081ea
aalokpokhrel123@gmail.com <aalokpokhrel123@GMAIL.COM>
2023-08-22 11:17:45 +0545

    java code

---
+++ Reservation/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+// static
+//    Default	declarations are visible only within the package (package private)
+//    Private	declarations are visible within the class only
+//Protected	declarations are visible within the package or all subclasses
+//    Public	declarations are visible everywhere
+// animal name
+    public static class Animal {
+        public String Name;
+
+    }
+
+    public static void main(String[] args)
+    {
+        Animal animal = new Animal();
+        System.out.println("Hello world!");
+    }
+}
